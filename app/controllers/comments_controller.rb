c7fdc9a757742c1c7d042d1aa66ef2624bef2eac
Gain Kim <lina.kim727@likelion.org>
2017-04-29 19:34:27 +0000

    Step11-Define 'create'action of commet to save user inputs in comments DB

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+    def create
+        comment = Comment.new
+        comment.posting_id = params[:id_of_posting]
+        comment.comment_content = params[:comment]
+        comment.save
+        
+        redirect_to '/index'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'postings#index'
   get  '/index' => 'postings#index'
   post '/postings/create' => 'postings#create'
+  post '/comments/create' => 'comments#create'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
